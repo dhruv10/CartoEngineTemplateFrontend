44b3c22cdaf8c816f78d545b0605c6a5bd6a1fcf
Dhruv Bhatnagar <ATLAN\dhruv.b@atdt023.atlan.lan>
2019-06-08 21:11:42 +0530

    Add layer, hide layer, show layer, update layer, remove layer implemented in single component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,77 +28,50 @@
           <!-- as-container code starts here -->
           <div class="as-container">
             <section class="as-box as-box--border">
-              <h1 class="as-title">Container 1</h1>
+              <h1 class="as-title">Add Layer</h1>
+              <button @click="addLayer()">
+                Add Layer
+              </button>
               <p class="as-body">
                 Use this container when you need to present fixed content. This content will occupy as
                 much vertical space as needed.
               </p>
             </section>
             <section class="as-box as-box--border">
-              <h1 class="as-title">Container 1</h1>
+              <h1 class="as-title">Hide Layer</h1>
+              <button @click="hideLayer()">
+                Hide Layer
+              </button>
               <p class="as-body">
                 Use this container when you need to present fixed content. This content will occupy as
                 much vertical space as needed.
               </p>
             </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
+            <section class="as-box as-box--border">
+              <h1 class="as-title">Show Layer</h1>
+              <button @click="showLayer()">
+                Show Layer
+              </button>
               <p class="as-body">
                 Use this container when you need to present fixed content. This content will occupy as
                 much vertical space as needed.
               </p>
             </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
+            <section class="as-box as-box--border">
+              <h1 class="as-title">Update Layer</h1>
+              <button @click="updateLayer()">
+                Update Layer
+              </button>
               <p class="as-body">
                 Use this container when you need to present fixed content. This content will occupy as
                 much vertical space as needed.
               </p>
             </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
-              <p class="as-body">
-                Use this container when you need to present fixed content. This content will occupy as
-                much vertical space as needed.
-              </p>
-            </section>
-            <section class="as-box">
-              <h1 class="as-title">Container 1</h1>
+            <section class="as-box as-box--border">
+              <h1 class="as-title">Remove Layer</h1>
+              <button @click="removeLayer()">
+                Remove Layer
+              </button>
               <p class="as-body">
                 Use this container when you need to present fixed content. This content will occupy as
                 much vertical space as needed.
@@ -142,16 +115,77 @@
 </template>
 
 <script>
+import carto from '@carto/carto-vl'
+
 export default {
   name: "HelloWorld",
-  data() {},
+  data() {
+    map = {},
+    source = {},
+    viz = {},
+    layer = {}
+  },
+  mounted() {
+    // Add basemap and set properties
+    this.map = new mapboxgl.Map({
+      container: "map",
+      style: carto.basemaps.voyager,
+      center: [0, 30],
+      zoom: 2
+    });
+
+    //** CARTO VL functionality begins here **//
+
+    // Define user
+    carto.setDefaultAuth({
+      username: "cartovl",
+      apiKey: "default_public"
+    });
+
+    // Define source
+    this.source = new carto.source.Dataset("populated_places");
+
+    // Define Viz object and custom style
+    this.viz = new carto.Viz(`
+            color: purple
+            width: 5
+        `);
+  },
   methods: {
     toggleDrawer() {
       console.log("kamikaze");
-    }
+    },
+    addLayer() {
+    // Define map layer
+    this.layer = new carto.Layer("layer", this.source, this.viz);
+
+    // Add map layer
+    this.layer.addTo(this.map);
+    },
+    hideLayer() {
+      this.layer.hide();
+    },
+    showLayer() {
+      this.layer.show();
+    },
+    updateLayer() {
+      this.viz = new carto.Viz(`
+            color: red
+            width: 8
+        `);
+      this.layer.update(this.source, this.viz);
+    },
+    removeLayer() {
+      this.layer.remove();
+    }   
   }
 };
 </script>
 
-<style>
+<style scoped>
+#map {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
 </style>
