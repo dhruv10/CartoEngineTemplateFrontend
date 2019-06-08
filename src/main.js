9f9a2d38bbc4cf84bb4871868708ed50decb808e
Dhruv Bhatnagar <ATLAN\dhruv.b@atdt023.atlan.lan>
2019-06-08 16:22:36 +0530

    importing all carto vl and  airship  stuff

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@carto/airship-style/dist/airship.css';
 import { defineCustomElements } from '@carto/airship-components/dist/loader';
+
 
 defineCustomElements(window);
 Vue.config.productionTip = false
-// Vue.config.ignoredElements = [/as-\w+/];
+Vue.config.ignoredElements = [/as-\w+/];
 
 
 new Vue({
